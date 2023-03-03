dbdc6bef0be5ec06f2adcd18b9b99be3aacee6ee
Martin Heermance <mheermance@gmail.com>
2023-03-03 15:40:06 -0500

    Added filtering input word list.

--- wordlebot/wordlebot.py
+++ wordlebot/wordlebot.py
@@ -6,15 +6,6 @@ def load_words(filename):
         words = sourcefile.read().splitlines()
         return words
 
-class Context:
-    """
-    A context block is used to hold search state between guesses
-    """
-    def __init__(self):
-        self.must_contain = ""
-        self.exclude = ""
-        self.candidate_words = []
-
 class Match:
     """
     An enum type to scores letters in a word.
@@ -24,37 +15,87 @@ class Match:
     Partial = 1
     Exact = 2
 
-def score_guess(guess, word, exclude):
+class Context:
+    """
+    A context block is used to hold search state between guesses
+    """
+    def __init__(self):
+        self.contains = ""
+        self.excludes = ""
+        self.candidate_words = []
+        self.word = [ '*', '*', '*', '*', '*' ]
+
+    def __str__(self):
+        retVal = ("contains=" + self.contains +
+                  ", excludes=" + self.excludes +
+                  ", word=" + str(self.word) +
+                  ", candidate_words=" + str(self.candidate_words))
+        return retVal
+
+def score_guess(guess, context):
     # Assume no matches
     retval = [Match.Miss, Match.Miss, Match.Miss, Match.Miss, Match.Miss]
 
     # Check for a match anywhere.
     for idx in range(5):
-        gchar = "" + guess[idx]
-        if word.find(gchar) != -1:
+        gchar = guess[idx]
+        if secret.find(gchar) != -1:
             retval[idx] = Match.Partial
 
     # Check for exact matches
     for idx in range(5):
-        if guess[idx] == word[idx]:
+        if guess[idx] == secret[idx]:
             retval[idx] = Match.Exact
-
-    # Check for excluded letters
-    for idx in range(5):
-        gchar = "" + word[idx]
-        if exclude.find(gchar) != -1:
-            retval[idx] = Match.Exclude
 
     return retval
 
-def filter_words(guess, words, exclude):
+def filter_word(word, context):
+    retval = True
+
+    # Check for excluded letters
+    for gchar in word:
+        if context.excludes.find(gchar) != -1:
+            return False
+
+    # Check that all contained letters are present.
+    for c in context.contains:
+        if word.find(c) == -1:
+            return False
+
+    # Check for any required letters.
+    for idx in range(5):
+        gchar = context.word[idx]
+        if gchar != '*' and word[idx] != gchar:
+            return False
+
+    return retval
+
+def filter_words(guess, words, context):
+    print("Guess = " + guess + ", input number of words = " + str(len(words)))
+    # First score the guess to enabling filtering
+    score = score_guess(guess, context)
+    print(score)
+
+    # Add each letter's score to appropriate filter criteria.
+    for idx in range(5):
+        # Add any misses to excluded letters.
+        if score[idx] == Match.Miss:
+            context.excludes += guess[idx]
+
+        # Add any partials to the contains list.
+        if score[idx] == Match.Partial:
+            context.contains += guess[idx]
+
+        # Add any exact to the target word.
+        if score[idx] == Match.Exact:
+            context.word[idx] = guess[idx]
+
+    # Now use the context to filter the word list.
     retval = []
     for word in words:
-        score = sum(score_guess(guess, word, exclude))
-        print(guess + " " + word + " " + str(score))
-        # a nonzero score means the word is a candidate
-        if (score > 0):
+        if filter_word(word, context):
             retval.append(word)
+    context.candidate_words = retval
     return retval
 
 # Create a global with all words.
@@ -62,5 +103,13 @@ words = load_words("words.txt")
 
 secret = "frost"
 
+ctx = Context()
+ctx.excludes = ""
+
 guess = "salet"
-print(filter_words(guess, words, "y"))
+filter_words(guess, words, ctx)
+print(ctx)
+filter_words(ctx.candidate_words[0], ctx.candidate_words, ctx)
+print(ctx)
+filter_words(ctx.candidate_words[0], ctx.candidate_words, ctx)
+print(ctx)
